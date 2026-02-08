16942cfb83c9082ca9398a3e812d27f3de74d789
p-jaswanth-kumar-reddy <pjaswanthkumarreddy3@gmail.com>
2026-02-08 22:06:23 +0530

    Day 4: Auth middleware and protected routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const protectedRoutes = require("./routes/protectedRoutes");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api", protectedRoutes);
 
 app.get("/health", (req, res) => {
   res.send("Server is running");
