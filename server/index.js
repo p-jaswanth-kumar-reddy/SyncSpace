8531f55d2a9bf6675eb8093c083d4239734ae934
p-jaswanth-kumar-reddy <pjaswanthkumarreddy3@gmail.com>
2026-02-08 15:00:33 +0530

    Day 1: SyncSpace project setup

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.send("Server is running");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(5000, () =>
+      console.log("Server running on port 5000")
+    );
+  })
+  .catch((err) => console.log(err));
